f77e4b98002140521da8d16119965e74d2d22e8e
oitsjustjose <stovallj1995@gmail.com>
2025-05-23 14:18:50 -0500

    Obnoxiously verbose logging at this point

--- src/model/mac-builder.ts
+++ src/model/mac-builder.ts
@@ -1,14 +1,36 @@
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
-import { existsSync, readFileSync } from 'node:fs';
+import { existsSync, readFileSync, renameSync } from 'node:fs';
+import { homedir } from 'node:os';
 import { Severity, UnityErrorParser } from './error/unity-error-parser';
 
 class MacBuilder {
   public static async run(actionFolder: string, silent: boolean = false): Promise<number> {
-    const buildLogPath = `unity-build.log`;
-    const runCommand = `bash ${actionFolder}/platforms/mac/entrypoint.sh | tee ${buildLogPath}`;
+    const buildLogPath = `${homedir()}/unity-build.log`;
 
+    core.info(`buildLogPath = ${buildLogPath}`);
+
+    if (existsSync(buildLogPath)) {
+      core.info(`Build log at ${buildLogPath} already exists -- moving it to ${buildLogPath}.old`);
+      renameSync(buildLogPath, `${buildLogPath}.old`); // renameSync will replace the existing file
+    }
+
+    core.info(`Touching ${buildLogPath} to ensure it exists`);
+    await exec(`touch ${buildLogPath}`);
+    if (existsSync(buildLogPath)) {
+      core.info(`Confirmed that ${buildLogPath} exists!`);
+    } else {
+      core.warning(`!!!${buildLogPath} does not exist after touching!!!`);
+    }
+
+    const runCommand = `bash ${actionFolder}/platforms/mac/entrypoint.sh | tee ${buildLogPath}`;
     const exitCode = await exec(runCommand, [], { silent, ignoreReturnCode: true });
+
+    if (existsSync(buildLogPath)) {
+      core.info(`Build log at ${buildLogPath} still exists after build!`);
+    } else {
+      core.warning(`!!!Build log at ${buildLogPath} no longer exists after build!!!`);
+    }
 
     if (UnityErrorParser.doErrorReporting && existsSync(buildLogPath)) {
       const logContent = readFileSync(buildLogPath, 'utf8');
